464872f839200e02c73ed87100380839908a8103
Brady Nolan <nolanb@gilmour.org>
2019-10-30 15:08:15 -0400

    obstacle

--- student.py
+++ student.py
@@ -85,7 +85,7 @@ class Piggy(PiggyParent):
         count = 0
         starting_position = self.get_heading()
         self.right(primary=60, counter=-60)
-        time.sleep(.5)
+        time.sleep(2)
         while abs(self.get_heading() - starting_position) < 1:
             if self.read_distance() < 250 and not found_something:
                 found_something = True
