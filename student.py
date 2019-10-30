748e367ae98b321c5fbe8f63fea88fff2013ddec
Brady Nolan <nolanb@gilmour.org>
2019-10-30 15:02:02 -0400

    obs

--- student.py
+++ student.py
@@ -85,7 +85,7 @@ class Piggy(PiggyParent):
         count = 0
         starting_position = self.get_heading()
         self.right(primary=60, counter=-60)
-        time.sleep(.1)
+        time.sleep(.5)
         while abs(self.get_heading() - starting_position) < 2:
             if self.read_distance() < 250 and not found_something:
                 found_something = True
