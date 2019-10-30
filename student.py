ecf5ad70214d3f448901ddfc023b8e2e46e20712
Brady Nolan <nolanb@gilmour.org>
2019-10-30 15:10:37 -0400

    see

--- student.py
+++ student.py
@@ -85,8 +85,8 @@ class Piggy(PiggyParent):
         count = 0
         starting_position = self.get_heading()
         self.right(primary=60, counter=-60)
-        time.sleep(2)
-        while abs(self.get_heading() - starting_position) < 1:
+        time.sleep(3)
+        while abs(self.get_heading() - starting_position) <= 1:
             if self.read_distance() < 250 and not found_something:
                 found_something = True
                 count += 1
