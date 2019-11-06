c6993f549070e9c256fadbdc60c00fdb2cf16efc
Brady Nolan <nolanb@gilmour.org>
2019-11-06 10:53:45 -0500

    pushhhhh

--- student.py
+++ student.py
@@ -103,29 +103,30 @@ class Piggy(PiggyParent):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
         while True:    
-            while self.read_distance() > 150:
+            while self.read_distance() > 250:
                 self.fwd()
-                time.sleep(0.1)
+                time.sleep(.01)
             self.stop()
-            self.scan()
-            # traversal
+            self.shakeHeadInDisgust()
+            self.scan()            
+            #traversal
             left_total = 0
             left_count = 0
             right_total = 0
             right_count = 0
             for ang, dist in self.scan_data.items():
-                if ang < self.MIDPOINT:
+                if ang < self.MIDPOINT: 
                     right_total += dist
                     right_count += 1
                 else:
                     left_total += dist
                     left_count += 1
-                left_avg = left_total / left_count
-                right_avg = right_total / right_count
-                if left_avg > right_avg:
-                    self.turn_by_deg(-45)
-                else:
-                    self.turn_by_deg(45)
+            left_avg = left_total / left_count
+            right_avg = right_total / right_count
+            if left_avg > right_avg:
+                self.turn_by_deg(-45)
+            else:
+                self.turn_by_deg(45)
 
     
     def checkdirection(self):
