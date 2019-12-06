1c59e4939b9ca57abab0292707363472693dfccd
Brady Nolan <nolanb@gilmour.org>
2019-12-06 09:30:56 -0500

    corrected nav and starting position

--- student.py
+++ student.py
@@ -135,27 +135,33 @@ class Piggy(PiggyParent):
                 time.sleep(.01)
             self.stop()
             self.corner_count += 1
-            if self.corner_count == 4:
-                self.turn_to_deg(self.getout)
-            #traversal
-            left_total = 0
-            left_count = 0
-            right_total = 0
-            right_count = 0
-            self.scan()
-            for ang, dist in self.scan_data.items():
-                if ang < self.MIDPOINT: 
-                    right_total += dist
-                    right_count += 1
+            if self.corner_count >= 4:
+                current_heading = self.get_heading()
+                # check if there's a path toward the exit
+                self.turn_to_deg(starting_position)
+                # but if that's not clear, face the opposite as I was when I started this mess
+                if not self.quick_check():
+                    self.turn_to_deg(current_heading + 180)
+            else:        
+                #traversal
+                left_total = 0
+                left_count = 0
+                right_total = 0
+                right_count = 0
+                self.scan()
+                for ang, dist in self.scan_data.items():
+                    if ang < self.MIDPOINT: 
+                        right_total += dist
+                        right_count += 1
+                    else:
+                        left_total += dist
+                        left_count += 1
+                left_avg = left_total / left_count
+                right_avg = right_total / right_count
+                if left_avg > right_avg:
+                    self.turn_by_deg(-45)
                 else:
-                    left_total += dist
-                    left_count += 1
-            left_avg = left_total / left_count
-            right_avg = right_total / right_count
-            if left_avg > right_avg:
-                self.turn_by_deg(-45)
-            else:
-                self.turn_by_deg(45)
+                    self.turn_by_deg(45)
 
     
     def checkdirection(self):
